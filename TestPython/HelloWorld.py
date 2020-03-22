f597db3894729281097589dfb97ea85471839607
slimmilan <slimmilan@gmail.com>
2020-03-23 01:09:10 +0530

    Test Commit from pycharm

---
+++ TestPython/HelloWorld.py
@@ -0,0 +1,16 @@
+print("Hello \"World\" 'Double'")
+print('Hello "World" \'Single\'')
+print(r"/root/find is also D:\user\Administrator\find")
+print('''this is the
+way we print our output
+early in the morning''')
+print('hello' + " world")
+print('hello' " world")
+print('hello', "world")
+h = 'hello'
+w = " world"
+print(h + w)
+#print(h w) #doesn't work like this
+
+print(h[1])
+print(w[-1])
